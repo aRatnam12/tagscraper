ae56ea2ce8e239bb7deaa044ce6e5183787fd437
aRatnam12 <ansh.ratnam@gmail.com>
2015-11-15 20:20:10 -0600

    renders html from server

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from flask import render_template
 from lxml import html
 import requests
 app = Flask(__name__)
 
 @app.route('/')
+def renderPage():
+  return render_template("index.html")
+
+@app.route('/webpage')
 def scrapeUrl(url):
-    page = requests.get(url)
-    tree = html.fromstring(page.content)
-    print tree
+  page = requests.get(url)
+  tree = html.fromstring(page.content)
+  print tree
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
